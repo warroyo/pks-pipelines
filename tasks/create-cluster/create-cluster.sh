2551c29fe362724cd8a7117a8dfcb235a321d8bd
warroyo <warroyo7199008@gmail.com>
2020-04-24 15:45:15 -0600

    remove unecessary command

--- tasks/create-cluster/create-cluster.sh
+++ tasks/create-cluster/create-cluster.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 set -eu
-cat /var/version && echo ""
-curl -L -s -o jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-chmod +x jq
 mv pks/pks-linux-* pks-cli
 chmod +x pks-cli
 ./pks-cli login -a $PKS_API -u $PKS_USERNAME -p $PKS_PASSWORD -k
