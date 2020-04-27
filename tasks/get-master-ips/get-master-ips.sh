82eba076693ff02ff8ee8833307825c4d96f62cb
warroyo <warroyo7199008@gmail.com>
2020-04-27 14:07:45 -0600

    add curl

--- tasks/get-master-ips/get-master-ips.sh
+++ tasks/get-master-ips/get-master-ips.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -eu
+apt-get update && apt-get install curl
 curl -L -s -o jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x jq
 mv pks/pks-linux-* pks-cli
