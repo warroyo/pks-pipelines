f1af200e113fe2cd921d05faebe1c0d6a1047f13
warroyo <warroyo7199008@gmail.com>
2020-04-28 10:43:30 -0600

    add vars file

--- terraform/master-dns/master-dns.tf
+++ terraform/master-dns/master-dns.tf
@@ -1,8 +1,5 @@
 
 provider "infoblox"{
-    INFOBLOX_USERNAME = var.infoblox_user
-    INFOBLOX_SERVER = var.infoblox_server
-    INFOBLOX_PASSWORD = var.infoblox_password
 }
 resource "infoblox_a_record" "master_dns"{
   ip_addr= var.vip_ip
---
+++ terraform/master-dns/variables.tf
@@ -0,0 +1,20 @@
+variable "vip_ip" {
+  type        = string
+  description = "the ip for the vip"
+}
+
+variable "pks_env" {
+  type        = string
+  description = "env name"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "cluster name"
+}
+
+
+variable "dns_zone" {
+  type        = string
+  description = "dns zone name"
+}
