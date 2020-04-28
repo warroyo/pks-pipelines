0800a2a24f822c6b396c088660cf613baff9b50a
warroyo <warroyo7199008@gmail.com>
2020-04-28 12:58:52 -0600

    fix dns naming

--- terraform/master-dns/master-dns.tf
+++ terraform/master-dns/master-dns.tf
@@ -2,8 +2,8 @@
 provider "infoblox"{
 }
 resource "infoblox_a_record" "master_dns"{
-  ip_addr= var.vip_ip
-  vm_name="${var.pks_env}-${var.cluster_name}-masters"
+  ip_addr = var.vip_ip
+  vm_name = var.cluster_name
   zone= var.dns_zone
   tenant_id = var.tenant_id
   cidr = var.cidr
--- terraform/master-dns/variables.tf
+++ terraform/master-dns/variables.tf
@@ -3,11 +3,6 @@ variable "vip_ip" {
   description = "the ip for the vip"
 }
 
-variable "pks_env" {
-  type        = string
-  description = "env name"
-}
-
 variable "cluster_name" {
   type        = string
   description = "cluster name"
