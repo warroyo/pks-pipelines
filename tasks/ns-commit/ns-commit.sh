6677953bb355ff2c2796aad94f6496b1c473a0e3
warroyo <warroyo7199008@gmail.com>
2020-04-28 10:25:37 -0600

    fix var name

--- tasks/ns-commit/ns-commit.sh
+++ tasks/ns-commit/ns-commit.sh
@@ -2,4 +2,4 @@
 
 set -eu
 apt-get update && apt-get install -y curl
-curl -k -XPOST -H 'Content-type: application/json' -H "X-NITRO-USER:${NS_USER}" -H "X-NITRO-PASS:${NS_PASSWORD}" ${NS_URL}/nitro/v1/config/nsconfig?action=save -d '{"nsconfig": {}}'
+curl -k -XPOST -H 'Content-type: application/json' -H "X-NITRO-USER:${NS_LOGIN}" -H "X-NITRO-PASS:${NS_PASSWORD}" ${NS_URL}/nitro/v1/config/nsconfig?action=save -d '{"nsconfig": {}}'
