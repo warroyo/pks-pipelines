a3737f0cea498673a5bf39a93cf2e9e00eaed7d4
warroyo <warroyo7199008@gmail.com>
2020-04-28 09:06:47 -0600

    fixing task yaml

--- terraform/citrix/create-vip.tf
+++ terraform/citrix/create-vip.tf
@@ -15,7 +15,7 @@ resource "citrixadc_servicegroup" "master-sg" {
   servicegroupname = "${var.pks_env}-${var.cluster_name}-masters-sg"
   lbvservers       = ["${citrixadc_lbvserver.master-lb.name}"]
   servicetype      = "TCP"
-  clttimeout       = "${var.backend_service_config["clttimeout"]}"
+  clttimeout       = var.backend_service_config["clttimeout"]
   servicegroupmembers = formatlist(
     "%s:%s",
     var.k8s_masters,
